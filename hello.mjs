22b9c69dd840ef34a5b9c3ab5479bf199b865a9e
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-12-18 10:08:05 +0000

    save

--- hello.mjs
+++ hello.mjs
@@ -1,8 +1,3 @@
-/*
-➜  new-keystatic-project curl 'https://sexzgsjhrqibssjfhhbz.supabase.co/rest/v1/todos' \
--H "apikey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNleHpnc2pocnFpYnNzamZoaGJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI4OTAwMzcsImV4cCI6MjAxODQ2NjAzN30.5KNv6CMvzUKXbn2zTWRWdPtRGMjIIsQuiv6Cr4yPqFs" \
--H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNleHpnc2pocnFpYnNzamZoaGJ6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMjg5MDAzNywiZXhwIjoyMDE4NDY2MDM3fQ.zuRH6XVKJr24GVqMHLMvTeb1JSAOGWYvUSvD4oZ_Jhw"
-*/
 import dotenv from 'dotenv'
 dotenv.config()
 import { createClient } from '@supabase/supabase-js'
